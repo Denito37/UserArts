9a0a27c1689ce96aa4f8da9a2b3d9e57d51abec7
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-12-29 23:44:11 -0500

    placeholder img adding system

--- js/page.js
+++ js/page.js
@@ -7,12 +7,48 @@ const block4 = document.createElement('div');
 const block5 = document.createElement('div');
 const block6 = document.createElement('div');
 
-let blockArray = [block];
-let newBlockArray= [block2,block3,block4,block5,block6];
-let count = 0;
+let blockArray = [block,block2,block3,block4,block5,block6];
+//let newBlockArray= [block2,block3,block4,block5,block6];
+//let count = 0;
 let countMsg = 0;
 
 addBlock();
+for(let j = 1; j< blockArray.length;j++){
+    blockArray[j].classList.add('hidden');
+}
+
+for(let i = 0; i<blockArray.length;i++){ //* update array length
+    document.querySelectorAll('#file')[i].addEventListener('change', function()  {
+        
+        const choosenImg = this.files[0];
+
+        if(choosenImg){
+            const reader = new FileReader();
+            reader.addEventListener('load', () =>{
+                blockArray[i].style.padding = "0rem";
+                blockArray[i].style.border = "0";
+                blockArray[i].innerHTML = '<img class = "image">';
+                if(i+1 < 6){
+                    blockArray[i+1].classList.remove('hidden');
+                }
+                document.querySelectorAll('.image')[i].src = reader.result;
+            });
+            reader.readAsDataURL(choosenImg);
+        }
+        countMsg += 1;
+        if(countMsg >= 6){errorMsg();}
+    /*
+        if(count <= 5){
+            blockArray.push(newBlockArray[count]);
+            addBlock();
+        }
+        count += 1;
+        countMsg += 1;
+        if(count >= 5){count = 4;}
+        if(countMsg >= 6){errorMsg();}
+        */
+    });
+}
 
 function addBlock(){
     for(let i = 0; i<blockArray.length;i++){
@@ -21,40 +57,16 @@ function addBlock(){
         main.appendChild(blockArray[i]);
     }
 }
-document.getElementById('file').addEventListener('change', function()  {
-    const choosenImg = this.files[0];
-    console.log('object');
-    if(choosenImg){
-        const reader = new FileReader();
-        reader.addEventListener('load', () =>{
-            console.log(reader);
-            blockArray[0].style.padding = "0rem";
-            blockArray[0].innerHTML = '<img class = "image">';
-            document.querySelector('.image').src = reader.result;
-        });
-        reader.readAsDataURL(choosenImg);
-    }
-
-    if(count <= 5){
-        blockArray.push(newBlockArray[count]);
-        addBlock();
-    }
-    count += 1;
-    countMsg += 1;
-    if(count >= 5){
-        count = 4;
-    }
-    if(countMsg >= 6){
-        const msg = document.createElement("div");
-        const msgTxt = document.createElement("p");
-        msgTxt.innerHTML = " LIMIT REACHED: EACH ALBUM CAN SET 6 IMAGES ";
-        main.appendChild(msg);
-        msg.appendChild(msgTxt);
-        msg.style.position = "absolute";
-        msg.style.top = "0";
-        msg.style.width = "100%";
-        msgTxt.style.color = "hsl(10,70%,80%)";
-        msgTxt.style.fontWeight = "700";
-        msgTxt.style.textAlign = "center";
-    }
-});
+function errorMsg(){
+    const msg = document.createElement("div");
+    const msgTxt = document.createElement("p");
+    msgTxt.innerHTML = " LIMIT REACHED: EACH ALBUM CAN SET 6 IMAGES ";
+    main.appendChild(msg);
+    msg.appendChild(msgTxt);
+    msg.style.position = "absolute";
+    msg.style.top = "0";
+    msg.style.width = "100%";
+    msgTxt.style.color = "hsl(10,70%,80%)";
+    msgTxt.style.fontWeight = "700";
+    msgTxt.style.textAlign = "center";    
+}
