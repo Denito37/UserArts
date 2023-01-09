65c2cf33326998061971e326a007b68d8b9c47af
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-01-09 18:54:32 -0500

    feature in development

--- js/album.js
+++ js/album.js
@@ -3,19 +3,68 @@ const polaroid = document.getElementById('polaroid');
 const postcard = document.getElementById('postcard');
 const button = document.getElementById('button');
 const trash = document.getElementById('trash');
+const custom = document.querySelector('.custom');
+const customFile = document.getElementById('change');
+const customExit = document.getElementById('exit');
 
-const block = document.createElement('div');
-const block2 = document.createElement('div');
-const block3 = document.createElement('div');
-const block4 = document.createElement('div');
-const block5 = document.createElement('div');
-const block6 = document.createElement('div');
-
-let blockArray = [block,block2,block3,block4,block5,block6];
+let blockArray = [];
 let itemsArray = localStorage.getItem('image') ? JSON.parse(localStorage.getItem('image')) : [];
-
-addBlock();
-
+// * LOOPS
+//* populate page with 6 blocks
+for(let i = 0 ; i < 6;i++){
+    addBlock();
+}
+// * reveals new block when image is added
+for(let j = 1; j< blockArray.length;j++){
+    blockArray[j].classList.add('hidden');
+}
+// * Repopulates page with elements saved in local storage
+for(let u = 0; u< itemsArray.length;u++){
+    if(u < 5){
+        blockArray[u + 1].classList.remove('hidden');
+    }
+    else{
+        errorMsg();
+    }
+    blockArray[u].style.padding = "0rem";
+    blockArray[u].style.border = "0";
+    blockArray[u].innerHTML = itemsArray[u];
+}
+// * adds click event to each block with an image to update values
+for(let i =0;i<itemsArray.length;i++){
+    blockArray[i].classList.add('edit')
+    blockArray[i].addEventListener('click', ()=>{
+        //custom.style.display = 'block'
+    })
+}
+// * Reads user image and appends it to image element
+for(let i = 0 ; i < blockArray.length - itemsArray.length;i++){
+    document.querySelectorAll('#file')[i].addEventListener('change', function()  {
+        const choosenImg = this.files[0];
+        if(choosenImg){
+            const reader = new FileReader();
+            reader.addEventListener('load', () =>{
+                addImage(blockArray[itemsArray.length]);
+                if(i+1 < 6){blockArray[i+1].classList.remove('hidden');}
+                if(itemsArray.length < 5){blockArray[itemsArray.length+1].classList.remove('hidden');}
+                if(blockArray[itemsArray.length].classList.contains("polaroid")){
+                    document.querySelectorAll('.polaroidImg')[itemsArray.length].src = reader.result;
+                }
+                if(blockArray[itemsArray.length].classList.contains("postcard")){
+                    document.querySelectorAll('.postcardImg')[itemsArray.length].src = reader.result;
+                }
+                if(blockArray[itemsArray.length].classList.contains("button")){
+                    document.querySelectorAll('.buttonImg')[itemsArray.length].src = reader.result;
+                }
+                //* save blocks to LS
+                itemsArray.push(blockArray[itemsArray.length].innerHTML);
+                localStorage.setItem('image', JSON.stringify(itemsArray));
+            });
+            reader.readAsDataURL(choosenImg);
+        }
+    });
+}
+//* Events
 window.addEventListener('load',() =>{
     activeToggle(polaroid,postcard,button);
     for(let i = 0; i < blockArray.length;i++){
@@ -80,58 +129,6 @@ button.addEventListener('click', () =>{
 })
 trash.addEventListener('click', clearImages);
 
-// * LOOPS
-// * reveals new block when image is added
-for(let j = 1; j< blockArray.length;j++){
-    blockArray[j].classList.add('hidden');
-}
-// * Repopulates page with elements saved in local storage
-for(let u = 0; u< itemsArray.length;u++){
-    if(u < 5){
-        blockArray[u + 1].classList.remove('hidden');
-    }
-    else{
-        errorMsg();
-    }
-    blockArray[u].style.padding = "0rem";
-    blockArray[u].style.border = "0";
-    blockArray[u].innerHTML = itemsArray[u];
-}
-// * adds click event to each block with an image to update values
-for(let i =0;i<itemsArray.length;i++){
-    blockArray[i].classList.add('edit')
-    blockArray[i].addEventListener('click', ()=>{
-        blockArray[i].childNodes[0].style.backgroundColor = '#656'
-    })
-}
-// * Reads user image and appends it to image element
-for(let i = 0 ; i < blockArray.length - itemsArray.length;i++){
-    document.querySelectorAll('#file')[i].addEventListener('change', function()  {
-        const choosenImg = this.files[0];
-        if(choosenImg){
-            const reader = new FileReader();
-            reader.addEventListener('load', () =>{
-                addImage(blockArray[itemsArray.length]);
-                if(i+1 < 6){blockArray[i+1].classList.remove('hidden');}
-                if(itemsArray.length < 5){blockArray[itemsArray.length+1].classList.remove('hidden');}
-                if(blockArray[itemsArray.length].classList.contains("polaroid")){
-                    document.querySelectorAll('.polaroidImg')[itemsArray.length].src = reader.result;
-                }
-                if(blockArray[itemsArray.length].classList.contains("postcard")){
-                    document.querySelectorAll('.postcardImg')[itemsArray.length].src = reader.result;
-                }
-                if(blockArray[itemsArray.length].classList.contains("button")){
-                    document.querySelectorAll('.buttonImg')[itemsArray.length].src = reader.result;
-                }
-                //* save blocks to LS
-                itemsArray.push(blockArray[itemsArray.length].innerHTML);
-                localStorage.setItem('image', JSON.stringify(itemsArray));
-            });
-            reader.readAsDataURL(choosenImg);
-        }
-    });
-}
-
 // * FUNCTIONS
 function gridCol(cols){
     main.style.gridTemplateColumns = 'repeat('+cols+',1fr)';
@@ -142,6 +139,8 @@ function activeToggle(item1,item2,item3){
     item3.classList.remove('active');    
 }
 function addBlock(){
+    const blok = document.createElement('div')
+    blockArray.push(blok);
     for(let i = 0; i<blockArray.length;i++){
         blockArray[i].classList.add('add');
         blockArray[i].innerHTML = '<input type="file" id="file"> <label for = file> <img src="../images/add.svg" alt="add-image"></label> <p>add image</p>';
@@ -156,10 +155,10 @@ function addImage(item){
     }
     if(item.classList.contains("postcard")){
         item.style.border = "0";
-        item.innerHTML = '<img class = "postcardImg">';
+        item.innerHTML = '<img class = "postcardImg"> ';
     }
     if(item.classList.contains("button")){
-        item.innerHTML = '<img class = "buttonImg">';
+        item.innerHTML = ' <img class = "buttonImg">';
     }     
     
 }
@@ -184,3 +183,29 @@ function errorMsg(){
         msg.style.display = 'none'
     })
 }
+
+custom.style.display = 'none'
+//* update image feature in developement
+/*
+customExit.addEventListener('click', () =>{
+    custom.style.display = 'none'
+})
+customFile.addEventListener('change', function(){
+    const choosenImg = this.files[0];
+    if(choosenImg){
+        const reader = new FileReader();
+        reader.addEventListener('load', () =>{
+            if(blockArray[itemsArray.length].classList.contains("polaroid")){
+                document.querySelectorAll('.polaroidImg')[0].src = reader.result; // * get target and the save to LS
+            }
+            if(blockArray[itemsArray.length].classList.contains("postcard")){
+                document.querySelectorAll('.postcardImg')[0].src = reader.result;
+            }
+            if(blockArray[itemsArray.length].classList.contains("button")){
+                document.querySelectorAll('.buttonImg')[0].src = reader.result;
+            }
+        });
+        reader.readAsDataURL(choosenImg);
+    }
+})
+*/
